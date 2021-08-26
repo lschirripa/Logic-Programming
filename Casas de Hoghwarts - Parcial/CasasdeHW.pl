3b96f4f3d9a6992ea72920f1a5b824db27c9b89f
lschirripa <lschirripa@frba.utn.edu.ar>
2021-08-26 16:51:53 -0300

    base de Conocimientos CDH

---
+++ Casas de Hoghwarts - Parcial/CasasdeHW.pl
@@ -0,0 +1,15 @@
+%%%%%%%%%%%%%%%%%%%%%%
+%BASE DE CONOCIMIENTOS
+%%%%%%%%%%%%%%%%%%%%%%
+
+%casa(NombreCasa)
+casa(slytherin).
+casa(hufflepuff).
+casa(ravenclaw).
+casa(gryffindor).
+
+sangre(harry, mestiza).
+sangre(draco, pura).
+sangre(hermione, impura).
+
+caracteristicas()
